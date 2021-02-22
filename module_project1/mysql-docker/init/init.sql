3ef0b429631ff32de7536fea7f22b3c2dea04811
Kenneth, Lee <edowon0623@gmail.com>
2021-02-22 16:12:05 +0900

    upload a material for reexp

--- module_project1/mysql-docker/init/init.sql
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-CREATE TABLE member(userid int, pwd varchar(10));
-=======
-CREATE TABLE member(userid varchar(20) primary key, pwd varchar(10), name varchar(20));
-
-INSERT INTO member(userid, pwd, name)
-VALUES('user1', 'user1', '사용자1');
->>>>>>> 8b280e030c759f32688d3c747155e110c6f4795a
